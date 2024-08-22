4215dd45f7bbb86f8920e055e3e483cd799c8eda
Harry Beaumont <harry.beaumont@hotmail.co.uk>
2024-08-22 09:41:57 +0100

    formatting

--- autoclicker.py
+++ autoclicker.py
@@ -4,6 +4,7 @@ import pynput
 
 logging.basicConfig(level=logging.INFO)
 
+
 class AutoClicker:
     logger = logging.getLogger()
     DELAY = 0.05
@@ -14,18 +15,20 @@ class AutoClicker:
         self.exit_flag.set()
 
     def run(self) -> None:
-        with pynput.keyboard.Listener(on_press=self.onPress) as self.listener:
-            with pynput.mouse.Listener(on_move=self.onMove) as self.mouse_listener:
-                self.start()
-                self.listener.join()
-                self.mouse_listener.join()
+        with (
+            pynput.keyboard.Listener(on_press=self.onPress) as self.listener,
+            pynput.mouse.Listener(on_move=self.onMove) as self.mouse_listener,
+        ):
+            self.start()
+            self.listener.join()
+            self.mouse_listener.join()
 
     def onMove(self, *args):
         if not self.exit_flag.is_set():
             self.stop()
 
     def onPress(self, key) -> None:
-        if key == pynput.keyboard.KeyCode(char='a'):
+        if key == pynput.keyboard.KeyCode(char="a"):
             self.logger.info("key pressed")
             self.logger.info(self.exit_flag.is_set())
             if self.exit_flag.is_set():
@@ -33,7 +36,7 @@ class AutoClicker:
             else:
                 self.stop()
 
-        if key == pynput.keyboard.KeyCode(char='b'):
+        if key == pynput.keyboard.KeyCode(char="b"):
             self.listener.stop()
             self.mouse_listener.stop()
 
